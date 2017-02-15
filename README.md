67628dd415b14be8389bbfe9877c2a8de1810fac
Enrico Deleo <enricodeleo@gmail.com>
2017-02-15 19:14:39 +0100

    going open

---
+++ README.md
@@ -0,0 +1,33 @@
+## Supported languages iOS Apache Cordova
+
+**Cordova / PhoneGap Plugin that exposes supported languages to the App Store (iTunes).**
+
+On iOS, Cordova declares (just) english as supported language. Since most of the i18n solutions that developers use for their translation/internazionalizations support are javascript based there is no way to say to app store that anoter language is also supported. You need to explicitly declare those languages within the `info.plist` file.
+
+Since I'm lazy I prefer letting a plugin do this job (avoiding the need to re-edit the file everytime I do a fresh build from the cordova cli). Hope you find it useful as well.
+
+## Install
+
+### Latest published version on npm (with Cordova CLI >= 5.0.0)
+
+```
+cordova plugin add cordova-plugin-ios-localized-strings --variable MAIN_LANGUAGE=English --variable ADDITIONAL_LANGUAGES=it,fr,de
+```
+
+### Latest version from GitHub
+
+```
+cordova plugin add https://github.com/enricodeleo/cordova-plugin-ios-localized-strings.git --variable MAIN_LANGUAGE=English --variable ADDITIONAL_LANGUAGES=it,fr,de
+```
+
+## Platforms
+
+Applies to iOS only.
+
+## Bonus
+
+For ionic/angular v1.x based projects I really like [gabegorelick/gulp-angular-gettext](https://github.com/gabegorelick/gulp-angular-gettext).
+
+## License
+
+[MIT License](http://ilee.mit-license.org)
---
+++ scripts/add_supported_languages.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+'use strict';
+
+// Add supported languages for App Store (iTunes)
+// v1.0.0
+// Automatically adds supported languages to your iOS app
+// within the info.plist file after the `prepare` command.
+
+const fs = require('fs');
+const path = require('path');
+const plist = require('plist');
+
+module.exports = context => {
+  const ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser;
+
+  return new Promise((resolve, reject) => {
+    let mainConfig = new ConfigParser(path.resolve(context.opts.projectRoot, 'config.xml'));
+    let name = mainConfig.name();
+    let config = new ConfigParser(path.resolve(context.opts.projectRoot, 'platforms', 'ios', name, 'config.xml'));
+    let MAIN_LANGUAGE = config.getGlobalPreference('MAIN_LANGUAGE');
+    let ADDITIONAL_LANGUAGES = config.getGlobalPreference('ADDITIONAL_LANGUAGES');
+    let plistPath = path.join(context.opts.projectRoot, 'platforms', 'ios', name, `${name}-Info.plist`);
+
+    fs.readFile(plistPath, 'utf-8', (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      let infoPlist = plist.parse(data);
+
+      process.stdout.write('[iOS] Setting main language to: ' + MAIN_LANGUAGE + '\n');
+      infoPlist.CFBundleDevelopmentRegion = MAIN_LANGUAGE;
+
+      if ( ADDITIONAL_LANGUAGES ) {
+        process.stdout.write('[iOS] Setting also additional languagees to: ' + ADDITIONAL_LANGUAGES.split(',') + '\n');
+        infoPlist.CFBundleLocalizations = ADDITIONAL_LANGUAGES.split(',');
+      }
+
+      fs.writeFile(plistPath, plist.build(infoPlist), err => {
+        if (err) {
+          return reject();
+        }
+        resolve();
+      });
+    });
+  });
+};
